364360f4a107f2335da847e4e276e9cb21a306b9
Duncan McPherson <d.mcpherson.home@gmail.com>
2025-06-16 22:26:48 -0600

    feat: introduce VbcInstance for class representation
    - Added `VbcInstance` to encapsulate class definitions and fields.
    - Provides a dictionary to manage dynamic object fields.

---
+++ Vectra.VM/Runtime/VbcInstance.cs
@@ -0,0 +1,15 @@
+﻿using Vectra.Bytecode.Models;
+
+namespace Vectra.VM.Runtime;
+
+public class VbcInstance
+{
+    public VbcClass ClassDef { get; }
+    public Dictionary<string, object> Fields { get; }
+    
+    public VbcInstance(VbcClass classDef)
+    {
+        ClassDef = classDef;
+        Fields = new Dictionary<string, object>();
+    }
+}
